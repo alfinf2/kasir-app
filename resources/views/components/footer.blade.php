fb90fcf0db2ce57e7e4f7982e06a20914968cca1
alfinf2 <alfinfahreza090404@gmail.com>
2025-06-20 14:23:24 +0700

    Kasir App Terbaru

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,7 +2,7 @@
         <div class="container mx-auto text-center">
             <p class="text-sm">&copy; {{ date('Y') }} Kasir App. All rights reserved.</p>
             <a href="https://github.com/alfinf2" class="hover:underline font-semibold">
-                Developed by Ubah Namanya
+                Developed by Alfin F
             </a>
         </div>
     </footer>
